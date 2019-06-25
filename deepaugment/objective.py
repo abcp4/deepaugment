a767c2122d29046c7a5cdc13ecd12b6a97e48992
Ivan Pereira <navi1921@gmail.com>
2019-06-25 19:56:28 -0300

    Update objective.py

--- deepaugment/objective.py
+++ deepaugment/objective.py
@@ -11,7 +11,7 @@ file_path = realpath(__file__)
 dir_of_file = dirname(file_path)
 sys.path.insert(0, dir_of_file)
 
-from augmenter import augment_by_policy
+from deepaugment.augmenter import augment_by_policy
 from lib.helpers import log_and_print
 
 import os
@@ -19,12 +19,12 @@ import torch
 import torch.nn as nn
 import numpy as np
 from tensorboardX import SummaryWriter
-from config import AugmentConfig
+#from deepaugment.config import AugmentConfig
 import utils
-from models.augment_cnn import AugmentCNN
+from deepaugment.models.augment_cnn import AugmentCNN
 
 
-config = AugmentConfig()
+#config = AugmentConfig()
 
 device = torch.device("cuda")
 
@@ -32,8 +32,8 @@ device = torch.device("cuda")
 writer = SummaryWriter(log_dir=os.path.join(config.path, "tb"))
 writer.add_text('config', config.as_markdown(), 0)
 
-logger = utils.get_logger(os.path.join(config.path, "{}.log".format(config.name)))
-config.print_params(logger.info)
+#logger = utils.get_logger(os.path.join(config.path, "{}.log".format(config.name)))
+#config.print_params(logger.info)
 
 
 
@@ -97,18 +97,21 @@ class Objective:
         init_channels = 24
         use_aux = True
         epochs = 30
+        lr = 0.01
+        momentum = 0.995
+        weight_decay = 0.995
         genotype = "Genotype(normal=[[('dil_conv_3x3', 0), ('sep_conv_5x5', 1)], [('sep_conv_3x3', 1), ('avg_pool_3x3', 0)],[('dil_conv_3x3', 1), ('dil_conv_3x3', 0)], [('sep_conv_3x3', 3), ('skip_connect', 1)]], normal_concat=range(2, 6), reduce=[[('sep_conv_3x3', 1), ('dil_conv_5x5', 0)], [('skip_connect', 0), ('sep_conv_5x5', 1)], [('sep_conv_5x5', 1),('sep_conv_5x5', 0)], [('max_pool_3x3', 1), ('sep_conv_3x3', 0)]], reduce_concat=range(2, 6))"
         model = AugmentCNN(self.input_size, self.input_channels, init_channels, self.n_classes, layers,
                        use_aux,genotype)
-        model = nn.DataParallel(model, device_ids=config.gpus).to(device)
+        model = nn.DataParallel(model, device_ids='0').to(device)
 
         # model size
         mb_params = utils.param_size(model)
         logger.info("Model size = {:.3f} MB".format(mb_params))
 
         # weights optimizer
-        optimizer = torch.optim.SGD(model.parameters(), config.lr, momentum=config.momentum,weight_decay=config.weight_decay)
-        
+        optimizer = torch.optim.SGD(model.parameters(), lr, momentum = momentum,weight_decay = weight_decay)
+        a  =2/0
         """
         for sample_no in range(1, self.opt_samples + 1):
             self.child_model.load_pre_augment_weights()
